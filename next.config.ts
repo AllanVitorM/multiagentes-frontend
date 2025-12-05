9ab1b4768875d6a1d828dd08c9751242466daadd
AllanVitorM <allanvitormarques14@gmail.com>
2025-12-05 02:58:03 -0300

    caminho absoluto

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        // Todas as requisições que começarem com /api/ serão reescritas
-        // para o seu backend na Railway.
-        source: "/api/:path*",
-        destination: "https://orquestradorbmad.up.railway.app/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
