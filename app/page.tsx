30c7703a75d61b12a091f65a02136d0983e8c954
AllanVitorM <allanvitormarques14@gmail.com>
2025-12-04 23:18:54 -0300

    test: testando handlelogin

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import Icon from "@/public/IconAvanade.svg";
@@ -12,52 +12,58 @@ export default function Home() {
   const [dados, setDados] = useState({ email: "", password: "" });
   const { login } = useAuth();
 
-
-
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-
+    console.log("handleLogin disparou");
     try {
       await login(dados);
       router.push("/orquestrador");
-    
     } catch (error: any) {
-      console.error("Credenciais inválidas.", error)
+      console.error("Credenciais inválidas.", error);
     }
   };
 
   const handleCadastro = () => {
-    router.push("/cadastro")
-  }
+    router.push("/cadastro");
+  };
 
   return (
     <div>
       <main className="flex w-full h-screen">
         <div className="w-1/3 h-screen bg-branco-avanade flex flex-col justify-center items-center gap-10">
           <Image src={Icon} alt="Icone" />
-          <form onSubmit={handleLogin} className="w-full flex flex-col justify-center items-center gap-5">
+          <form
+            onSubmit={handleLogin}
+            className="w-full flex flex-col justify-center items-center gap-5"
+          >
             <input
               type="email"
               className="w-md h-10 bg-white border-black border-2  rounded-md p-2"
               placeholder="Email"
               value={dados.email}
-              onChange={(e) => setDados({...dados, email: e.target.value})}
+              onChange={(e) => setDados({ ...dados, email: e.target.value })}
             />
             <input
               type="password"
               className="w-md h-10 bg-white border-2 border-black rounded-md p-2"
               placeholder="Password"
               value={dados.password}
-              onChange={(e) => setDados({...dados, password: e.target.value})}
+              onChange={(e) => setDados({ ...dados, password: e.target.value })}
             />
-            <button type="submit" className="w-60 h-10 rounded-md border-2 border-black hover:bg-gray-800 hover:text-white cursor-pointer">
+            <button
+              type="submit"
+              className="w-60 h-10 rounded-md border-2 border-black hover:bg-gray-800 hover:text-white cursor-pointer"
+            >
               Login
             </button>
           </form>
           <hr className="w-md" />
           <div className="flex flex-col justify-center items-center gap-6">
             <p>Não tem conta?</p>
-            <button className="w-52 border-2 h-10 border-black rounded-md hover:bg-gray-800 hover:text-white cursor-pointer" onClick={handleCadastro}>
+            <button
+              className="w-52 border-2 h-10 border-black rounded-md hover:bg-gray-800 hover:text-white cursor-pointer"
+              onClick={handleCadastro}
+            >
               Comece agora
             </button>
           </div>
