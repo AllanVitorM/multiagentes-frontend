09729fffdb0f1aebeb75ee83e1cf817eb1146fbb
AllanVitorM <allanvitormarques14@gmail.com>
2025-12-05 02:21:38 -0300

    mudanças na api

--- service/api.ts
+++ service/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_IP}`,
-    withCredentials: true
+  baseURL: "/api",
+  withCredentials: true,
 });
 
 export default api;
